04c55a6d0a571f961a25e7807b00a92b63bd8df2
midchll <deeversmitch@gmail.com>
2026-04-22 00:23:23 -0700

    MongoDB setup

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 var express = require('express');
-var app = express();
+const path = require('path');
+const { connectDB } = require('./src/db/mongo');
 
+var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// putting all static files in public/
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Connecting to db instance, server only starts listening if mongo connects
+async function startServer() {
+    await connectDB();
+    app.listen(8080, function() {
+        console.log('Server running on http://localhost:8080');
+    });
+}
 
 //Pages
+// Temporarily setting communities.html to root since no auth yet
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/communities.html');
+});
+/* ^^^
 app.get('/communities-page', (req, res) => {
     res.sendFile(__dirname + '/public/pages/communities.html');
 });
+*/
 
 //Routes
 app.use('/communities', require('./src/routes/communities'));
 
-
-app.listen(8080, function() {
-    console.log('Server running on http://localhost:8080');
-});
+startServer();
---
+++ src/db/communityQueries.js
@@ -0,0 +1,7 @@
+// community db queries
+
+const { getDB } = require('./mongo');
+
+// TODO: create/find communities and other communityCollection mongo queries
+
+module.exports = {};
