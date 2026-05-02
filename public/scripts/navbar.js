11272efa3ceb9b7182a9bc197398b7f931e786aa
midchll <deeversmitch@gmail.com>
2026-05-02 15:34:47 -0700

    nav prog

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -2,6 +2,30 @@ function $(id) {
     return document.getElementById(id);
 }
 
+function handleLogoutClicked() {
+    try {
+        const response = fetch('/auth/logout');
+        if (!response.message) {
+            console.log('Error trying to logout, you may not be signed in.');
+            return;
+        }
+    } catch (err) { console.log('Server error while logging out:', err); }
+}
+
+async function handleCommunitiesClicked() {
+    try {
+        const response = await fetch('/communities');
+        if (!response.ok) {
+            console.log('Error trying to get communities page');
+            return;
+        }
+    } catch (err) { console.log('Server error trying to load communities page:', err); }
+}
+
+async function handleProfileClicked() {
+    // IMPLEMENT
+}
+
 async function renderNavbar() {
     const navlinks = $("nav-links")
     if (!navlinks) return;
@@ -9,32 +33,35 @@ async function renderNavbar() {
 
     try {
         const response = await fetch("/session");
-        if (!response.user || !response.ok) {
+        const session = await response.json();
+
+        if (!response.ok || !session.user) {
             // User not logged in -> redirect to login at root
             window.location.href="/login";
             return;
         }
 
-        const session = await response.json()
-        const user = session.user;
+        const user = session.user; // potentially will use for profile page/name render in navbar
 
-        const links = [];
-        links.push({ label: "Communities", action: "/communitiespage" });
-        links.push({ label: "Logout", action: "/logout" });
+        const communitiesLink = document.createElement("button");
+        communitiesLink.innerText = 'Communities';
+        communitiesLink.addEventListener("click", handleCommunitiesClicked);
 
-        links.forEach((link) => {
-           const navItem = document.createElement("div");
-           navItem.innerHTML = `<button data-btn="${link.label}">${link.label}</button>`
-           navItem.addEventListener("click", () => {
-            try {
-                const response = fetch(link.action);
-                if (link.action === "/logout") window.location.href = "/";
-            } catch (err) { console.log(err) }
-           });
-           navlinks.appendChild(navItem);
-        });
+        const logoutLink = document.createElement("button");
+        logoutLink.innerText = 'Logout';
+        logoutLink.addEventListener("click", handleLogoutClicked);
+
+        const profileLink = document.createElement("button");
+        profileLink.innerText = "Profile (WIP)";
+        profileLink.addEventListener("click", handleProfileClicked);
+
+        navlinks.appendChild(communitiesLink);
+        navlinks.appendChild(logoutLink);
+        navlinks.appendChild(profileLink);
+
+
     } catch (err) {
-        console.error("Failed to load nav:", err);
+        console.err("Error rendering navbar");
     }
 }
 
